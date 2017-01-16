b368a31c6980d002cc5cd9fb9dddf8d0dcf97698
coder-pig <511810970@qq.com>
2017-01-16 14:46:53 +0800

    简历首页！

---
+++ Code/Controllers/AboutControllers.js
@@ -0,0 +1,13 @@
+/**
+ * Created by 0.0 on 2017/1/15.
+ */
+function AboutController($scope){
+    $scope.data = {
+        "data":{
+            "name":"lht",
+            "email":"511810970@qq.com"
+        }
+    };
+}
+
+
